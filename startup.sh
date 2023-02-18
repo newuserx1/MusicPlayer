571470a54f3307a1b1c33f3c429c59a24a08425a
newuserx1 <121651688+newuserx1@users.noreply.github.com>
2023-02-18 10:44:01 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,4 @@
-echo ">> FETCHING UPSTREAM..."
-git clone https://github.com/newuserx1/MusicPlayer /MusicPlayer
-echo ">> INSTALLING REQUIREMENTS..."
-cd /MusicPlayer
-pip3 install -U -r requirements.txt
-echo ">> STARTING MUSIC PLAYER USERBOT..."
-clear
+
 echo "
 #     #                        
 ##   ## #    #  ####  #  ####  
